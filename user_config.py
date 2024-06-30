ae5e9afd6129b852cd026df6479f153aeb7db22a
Aleyna Sütbaş <aleyna.sutbas@comstruct.com>
2024-06-30 18:16:01 +0200

    change pipeline type

--- user_config.py
+++ user_config.py
@@ -1,1 +1,1 @@
-TYPE = "embeddings" # Choose one of "embeddings", "all-to-en", "all-to-all", "all-to-en-tags"
+TYPE = "all-to-en" # Choose one of "embeddings", "all-to-en", "all-to-all", "all-to-en-tags"
